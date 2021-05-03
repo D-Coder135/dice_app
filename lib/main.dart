7e130f0d6d1f29349abdbfe0b7f76c0daf2d46f5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-03 19:56:11 +0530

    Add a new class name DicePage and extends StatefulWidget in it

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,18 @@ void main() {
   );
 }
 
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 // class DicePage extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
