4f410ff78d1a539fbdbea37690a067d13eb82bab
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-03 19:57:16 +0530

    Copy the code from the old build() function to the build() function of the new DicePage class

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,40 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // Expanded widget rearranges it child to occupy as much space as possible on the screen.
+            //  child: Image(
+            //   image: AssetImage('images/dice1.png'),
+            // ),
+
+            // Alternative & more appropriate way is as follows -->
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              // FlatButton widget comes already with padding therefore there is no need of adding extra padding.
+              onPressed: () {
+                print('Left button is pressed');
+              }, // This is known as void callback. It is similar to anonymous functions in other programming language
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          // SizedBox(
+          //   width: 16.0,
+          // ),
+          Expanded(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+                // FlatButton widget comes already with padding therefore there is no need of adding extra padding.
+                onPressed: () {
+                  print('Right button is pressed.');
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
 
