7c6451228058a1874c134674f6c6e112663110b6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-03 19:58:39 +0530

    Initialize a variable inside the class DicePageState

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,8 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
